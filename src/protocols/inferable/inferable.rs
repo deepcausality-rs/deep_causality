7b93f053d71d4ffde9ae46121e3f113f7d5198d5
Marvin Hansen <marvin.hansen@gmail.com>
2023-06-21 16:04:41 +0800

    Moved methods from Inference to Inferable trait as Default implementation.

--- src/protocols/inferable/inferable.rs
+++ src/protocols/inferable/inferable.rs
@@ -2,19 +2,54 @@
  * Copyright (c) 2023. Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
  */
 
+use std::cmp::Ordering;
 use std::fmt::Debug;
 
 use crate::prelude::{DescriptionValue, Identifiable, NumericalValue};
+use crate::utils::math_utils::abs_num;
 
 pub trait Inferable: Debug + Identifiable
 {
+    /// NumericalValue must be specified in the implementation
+    /// and adhere to the outlined type constraints of PartialOrd.
     type NumericValue: PartialOrd;
+
     fn question(&self) -> DescriptionValue;
     fn observation(&self) -> NumericalValue;
     fn threshold(&self) -> NumericalValue;
     fn effect(&self) -> NumericalValue;
     fn target(&self) -> NumericalValue;
-    fn conjoint_delta(&self) -> NumericalValue;
-    fn is_inferable(&self) -> bool;
-    fn is_inverse_inferable(&self) -> bool;
+
+    fn conjoint_delta(&self) -> NumericalValue {
+        abs_num((1.0) - self.observation())
+    }
+
+    fn is_inferable(&self) -> bool {
+        if (self.observation().total_cmp(&self.threshold()) == Ordering::Greater)
+            && approx_equal(self.effect(), self.target(), 4) {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn is_inverse_inferable(&self) -> bool {
+        if (self.observation().total_cmp(&self.threshold()) == Ordering::Less)
+            && approx_equal(self.effect(), self.target(), 4) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+
+// Because floats vary in precision, equality is not guaranteed.
+// Therefore, this comparison checks for approximate equality up to a certain number
+// of decimal places.
+fn approx_equal(a: f64, b: f64, decimal_places: u8) -> bool {
+    let factor = 10.0f64.powi(decimal_places as i32);
+    let a = (a * factor).trunc();
+    let b = (b * factor).trunc();
+    a == b
 }
--- src/types/reasoning_types/inferable/inference.rs
+++ src/types/reasoning_types/inferable/inference.rs
@@ -6,7 +6,6 @@ use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 
 use crate::prelude::*;
-use crate::utils::math_utils::abs_num;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Inference {
@@ -55,28 +54,6 @@ impl Inferable for Inference
     fn target(&self) -> NumericalValue {
         self.target
     }
-
-    fn conjoint_delta(&self) -> NumericalValue {
-        abs_num((1.0) - self.observation)
-    }
-
-    fn is_inferable(&self) -> bool {
-        if (self.observation.total_cmp(&self.threshold) == Ordering::Greater)
-            && approx_equal(self.effect, self.target, 4) {
-            true
-        } else {
-            false
-        }
-    }
-
-    fn is_inverse_inferable(&self) -> bool {
-        if (self.observation.total_cmp(&self.threshold) == Ordering::Less)
-            && approx_equal(self.effect, self.target, 4) {
-            true
-        } else {
-            false
-        }
-    }
 }
 
 
@@ -85,19 +62,8 @@ impl Display for Inference
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Inference:\
-               \n id: {},\n question: {},\n observation: {},\n threshold: {},\n effect: {}",
+            "Inference: \n id: {},\n question: {},\n observation: {},\n threshold: {},\n effect: {}",
             self.id, self.question, self.observation, self.threshold, self.effect
         )
     }
-}
-
-// Because floats vary in precision, equality is not guaranteed.
-// Therefore, this comparison checks for approximate equality up to a certain number
-// of decimal places.
-fn approx_equal(a: f64, b: f64, decimal_places: u8) -> bool {
-    let factor = 10.0f64.powi(decimal_places as i32);
-    let a = (a * factor).trunc();
-    let b = (b * factor).trunc();
-    a == b
 }
