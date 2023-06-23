7b9ccfbf086faa98589d0c74f6d59bdcb10e7297
Marvin Hansen <marvin.hansen@gmail.com>
2023-06-23 13:58:21 +0800

    Added Adjustable trait bound to Datable

--- src/protocols/contextuable/datable.rs
+++ src/protocols/contextuable/datable.rs
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2023. Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
  */
-use crate::prelude::Identifiable;
+use crate::prelude::{Adjustable, Identifiable};
 
-pub trait Datable: Identifiable {}
+pub trait Datable: Adjustable + Identifiable {}
