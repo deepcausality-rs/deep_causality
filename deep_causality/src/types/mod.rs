6be04b3fd6fc7fc20c436f676017e2abf1efe1ef
Marvin Hansen <marvin.hansen@gmail.com>
2023-06-26 17:39:09 +0800

    Removed unnecessary folder.

--- deep_causality/src/types/mod.rs
+++ deep_causality/src/types/mod.rs
@@ -4,7 +4,6 @@ pub mod alias_types;
 pub mod context_types;
 pub mod graph_types;
 pub mod grid_types;
-pub mod hyper_graph_types;
 pub mod model_types;
 pub mod reasoning_types;
 pub mod window_type;
