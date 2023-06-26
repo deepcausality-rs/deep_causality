5db24bf11570ff4cf658d40aae625f4e6575730a
Marvin Hansen <marvin.hansen@gmail.com>
2023-06-26 16:40:13 +0800

    Added some docstring.

--- deep_causality/src/types/window_type/sliding_window.rs
+++ deep_causality/src/types/window_type/sliding_window.rs
@@ -5,7 +5,7 @@
 use std::marker::PhantomData;
 use crate::prelude::{ArrayStorage, VectorStorage, WindowStorage};
 
-pub fn new_with_vector_storage<T: PartialEq + Copy + Default >(
+pub fn new_with_vector_storage<T: PartialEq + Copy + Default>(
     size: usize,
     multiple: usize,
 )
@@ -16,11 +16,9 @@ pub fn new_with_vector_storage<T: PartialEq + Copy + Default >(
     )
 }
 
-pub fn new_with_array_storage<T: PartialEq + Copy + Default ,  const SIZE: usize,const CAPACITY: usize>(
-)
+pub fn new_with_array_storage<T: PartialEq + Copy + Default, const SIZE: usize, const CAPACITY: usize>()
     -> SlidingWindow<ArrayStorage<T, SIZE, CAPACITY>, T>
 {
-
     assert!(CAPACITY > SIZE);
 
     SlidingWindow::with_storage(
@@ -30,7 +28,7 @@ pub fn new_with_array_storage<T: PartialEq + Copy + Default ,  const SIZE: usize
 
 pub struct SlidingWindow<S, T>
     where
-        T: PartialEq + Copy + Default ,
+        T: PartialEq + Copy + Default,
         S: WindowStorage<T>,
 {
     storage: S,
@@ -39,7 +37,7 @@ pub struct SlidingWindow<S, T>
 
 impl<S, T> SlidingWindow<S, T>
     where
-        T: PartialEq + Copy + Default ,
+        T: PartialEq + Copy + Default,
         S: WindowStorage<T>,
 {
     pub(crate) fn with_storage(storage: S) -> Self
@@ -54,44 +52,46 @@ impl<S, T> SlidingWindow<S, T>
         T: PartialEq + Copy + Default,
         S: WindowStorage<T>,
 {
+    /// Pushes a new element to the beginning of the sliding window.
+    /// If the window is filled, the last element will be dropped.
     pub fn push(&mut self, value: T)
     {
         self.storage.push(value)
     }
-
+    /// Returns the first element in the sliding window
     pub fn first(&self) -> Result<T, String>
     {
         self.storage.first()
     }
-
+    /// Returns the last element in the sliding window
     pub fn last(&self) -> Result<T, String>
     {
         self.storage.last()
     }
-
+    /// Returns true if the window is empty.
     pub fn empty(&self) -> bool
     {
         self.storage.empty()
     }
-
+    /// Returns true if the window is filled.
     pub fn filled(&self) -> bool
     {
         self.storage.filled()
     }
-
+    /// Returns the window size
     pub fn size(&self) -> usize
     {
         self.storage.size()
     }
-
+    /// Returns the sliding window as a fixed size static array.
     pub fn arr<const SIZE: usize>(&self) -> Result<[T; SIZE], String> {
         self.storage.arr()
     }
-
+    /// Returns sliding window as slice
     pub fn slice(&self) -> Result<&[T], String> {
         self.storage.slice()
     }
-
+    /// Returns the sliding window as a vector.
     pub fn vec(&self) -> Result<Vec<T>, String> {
         self.storage.vec()
     }
