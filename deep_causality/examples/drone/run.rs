209ff0292992a427c95db65fb722d24005cc8313
Marvin Hansen <marvin.hansen@gmail.com>
2023-06-28 12:46:00 +0800

    Updated examples to use new make_run macro

--- deep_causality/examples/drone/run.rs
+++ deep_causality/examples/drone/run.rs
@@ -1,9 +1,6 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
-use deep_causality_macros::make_run;
+pub fn run()
+{
 
-mod run;
-
-fn main() {
-    make_run!();
 }
