53a89f82e68a550d24b5b3d3deaabb07def8e2ed
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-06 17:22:04 +0800

    Fixed broken benchmarks.

--- deep_causality/benches/bench_main.rs
+++ deep_causality/benches/bench_main.rs
@@ -7,11 +7,8 @@ mod benchmarks;
 // In case of SIGSEGV: invalid memory reference,
 // just reduce sample size in the linear or multi cause graph benchmarks.
 criterion_main! {
-    benchmarks::bench_grid_array::array_grid,
     benchmarks::bench_collection::causality_collection,
     benchmarks::bench_map::causality_map,
     benchmarks::bench_linear_graph::linear_graph,
     benchmarks::bench_multi_cause_graph::multi_layer_graph,
-    benchmarks::bench_window_arr::window_array_backed,
-    benchmarks::bench_window_vec::window_vector_backed,
 }
