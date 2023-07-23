caa085c76b7f20384a51b4198e344e8ac28b6fba
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-23 19:17:59 +0800

    Added tests for assumable array.

---
+++ deep_causality/tests/assumable_arr_tests.rs
@@ -0,0 +1,217 @@
+// Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
+
+use deep_causality::protocols::assumable::AssumableReasoning;
+use deep_causality::utils::test_utils::{get_test_assumption_arr, get_test_assumption_vec, get_test_num_array};
+
+#[test]
+fn test_all_assumptions_tested()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+}
+
+#[test]
+fn test_all_assumptions_valid()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(all_valid);
+}
+
+#[test]
+fn test_percent_assumption_valid()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 0.0);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(all_valid);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 100.0);
+}
+
+#[test]
+fn test_get_all_invalid_assumptions()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 0.0);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_invalid = col.get_all_invalid_assumptions();
+    assert_eq!(all_invalid.len(), 0);
+}
+
+#[test]
+fn test_get_all_valid_assumptions()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 0.0);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_valid = col.get_all_valid_assumptions();
+    assert_eq!(all_valid.len(), 3);
+}
+
+#[test]
+fn test_get_all_tested_assumptions()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 0.0);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_tested_assumptions = col.get_all_tested_assumptions();
+    assert_eq!(all_tested_assumptions.len(), 3);
+}
+
+#[test]
+fn test_get_all_untested_assumptions()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let all_untested = col.get_all_untested_assumptions();
+    assert_eq!(all_untested.len(), 3);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 0.0);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_untested = col.get_all_untested_assumptions();
+    assert_eq!(all_untested.len(), 0);
+}
+
+#[test]
+fn test_verify_all_assumptions()
+{
+    let col= get_test_assumption_arr();
+    assert_eq!(col.len(), 3);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(!all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(!all_valid);
+
+    let all_valid_percent = col.percent_assumption_valid();
+    assert_eq!(all_valid_percent, 0.0);
+
+    let data = get_test_num_array();
+    col.verify_all_assumptions(&data);
+
+    let all_tested = col.all_assumptions_tested();
+    assert!(all_tested);
+
+    let all_valid = col.all_assumptions_valid();
+    assert!(all_valid);
+}
+
+#[test]
+fn test_get_all_items() {
+    let col = get_test_assumption_vec();
+    let all_items = col.get_all_items();
+
+    let exp_len = col.len();
+    let actual_len = all_items.len();
+    assert_eq!(exp_len, actual_len);
+}
+
+#[test]
+fn test_len() {
+    let col = get_test_assumption_vec();
+    assert_eq!(3, col.len());
+}
+
+#[test]
+fn test_is_empty() {
+    let col = get_test_assumption_vec();
+    assert!(!col.is_empty());
+}
--- deep_causality/tests/mod.rs
+++ deep_causality/tests/mod.rs
@@ -1,9 +1,11 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
 #[cfg(test)]
-mod assumable_vec_tests;
+mod assumable_arr_tests;
 #[cfg(test)]
 mod assumable_map_tests;
+#[cfg(test)]
+mod assumable_vec_tests;
 #[cfg(test)]
 mod assumption_tests;
 #[cfg(test)]
