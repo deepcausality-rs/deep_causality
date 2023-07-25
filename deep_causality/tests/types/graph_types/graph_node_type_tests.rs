8152ff11d95ea7b2d0350ed44bd8a46618d3f9ef
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-25 19:16:03 +0800

    Added tests for graph node type.

---
+++ deep_causality/tests/types/graph_types/graph_node_type_tests.rs
@@ -0,0 +1,133 @@
+// Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
+
+use deep_causality::prelude::{Dataoid, Node, NodeType, Root, Spaceoid, SpaceTempoid, Tempoid, TimeScale};
+
+#[test]
+fn test_root_some()
+{
+    let id = 1;
+    let root = Root::new(id);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Root(root));
+    assert!(node.vertex_type().root().is_some());
+    //
+    assert!(node.vertex_type().dataoid().is_none());
+    assert!(node.vertex_type().tempoid().is_none());
+    assert!(node.vertex_type().spaceiod().is_none());
+    assert!(node.vertex_type().space_tempoid().is_none());
+}
+
+#[test]
+fn test_root_none()
+{
+    let id = 1;
+    let data = 42;
+    let d = Dataoid::new(id, data);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Datoid(d));
+    assert!(node.vertex_type().root().is_none());
+}
+
+#[test]
+fn test_dataoid_some()
+{
+    let id = 1;
+    let data = 42;
+    let d = Dataoid::new(id, data);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Datoid(d));
+    assert!(node.vertex_type().dataoid().is_some());
+    //
+    assert!(node.vertex_type().root().is_none());
+    assert!(node.vertex_type().tempoid().is_none());
+    assert!(node.vertex_type().spaceiod().is_none());
+    assert!(node.vertex_type().space_tempoid().is_none());
+}
+
+#[test]
+fn test_dataoid_none()
+{
+    let id = 1;
+    let root = Root::new(id);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Root(root));
+    assert!(node.vertex_type().dataoid().is_none());
+}
+
+#[test]
+fn test_tempoid_some()
+{
+    let id = 1;
+    let time_scale = TimeScale::Month;
+    let time_unit = 1;
+
+    let tempoid = Tempoid::new(id, time_scale, time_unit);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Tempoid(tempoid));
+    assert!(node.vertex_type().tempoid().is_some());
+    //
+    assert!(node.vertex_type().dataoid().is_none());
+    assert!(node.vertex_type().root().is_none());
+    assert!(node.vertex_type().spaceiod().is_none());
+    assert!(node.vertex_type().space_tempoid().is_none());
+}
+
+#[test]
+fn test_tempoid_none()
+{
+    let id = 1;
+    let root = Root::new(id);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Root(root));
+    assert!(node.vertex_type().tempoid().is_none());
+}
+
+#[test]
+fn test_spaceiod_some()
+{
+    let id = 1;
+    let x = 7;
+    let y = 8;
+    let z = 9;
+
+    let d = Spaceoid::new(id, x, y, z);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Spaceiod(d));
+    assert!(node.vertex_type().spaceiod().is_some());
+    //
+    assert!(node.vertex_type().dataoid().is_none());
+    assert!(node.vertex_type().root().is_none());
+    assert!(node.vertex_type().tempoid().is_none());
+    assert!(node.vertex_type().space_tempoid().is_none());
+}
+
+#[test]
+fn test_spaceiod_none()
+{
+    let id = 1;
+    let root = Root::new(id);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Root(root));
+    assert!(node.vertex_type().spaceiod().is_none());
+}
+
+#[test]
+fn test_space_tempoid_some()
+{
+    let id = 1;
+    let time_scale = TimeScale::Month;
+    let time_unit = 1;
+    let x = 7;
+    let y = 8;
+    let z = 9;
+
+    let d = SpaceTempoid::new(id, time_scale, time_unit, x, y, z);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::SpaceTempoid(d));
+    assert!(node.vertex_type().space_tempoid().is_some());
+    //
+    assert!(node.vertex_type().dataoid().is_none());
+    assert!(node.vertex_type().root().is_none());
+    assert!(node.vertex_type().tempoid().is_none());
+    assert!(node.vertex_type().spaceiod().is_none());
+}
+
+#[test]
+fn test_space_tempoid_none()
+{
+    let id = 1;
+    let root = Root::new(id);
+    let node: Node<Dataoid, Spaceoid, Tempoid, SpaceTempoid> = Node::new(id, NodeType::Root(root));
+    assert!(node.vertex_type().space_tempoid().is_none());
+}
