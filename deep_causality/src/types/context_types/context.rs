21a9624974b508c7d122cde9e0cb36bdd0d826c8
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-25 16:51:03 +0800

    Added debug & display implementation to Context.

--- deep_causality/src/types/context_types/context.rs
+++ deep_causality/src/types/context_types/context.rs
@@ -1,6 +1,7 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
 
+use std::fmt::{Debug, Display, Formatter};
 use crate::prelude::{ContextMatrixGraph};
 use crate::protocols::contextuable::{Datable, SpaceTemporal, Spatial, Temporal};
 
@@ -48,5 +49,39 @@ impl<D, S, T, ST> Context<D, S, T, ST>
 
     pub fn edge_count(&self) -> usize {
         self.graph.edge_count()
+    }
+
+    fn format(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f,
+               "Context: id: {}, name: {}, node_count: {}, edge_count: {}",
+               self.id,
+               self.name,
+               self.node_count(),
+               self.edge_count(),
+        )
+    }
+}
+
+impl<D, S, T, ST> Debug for Context<D, S, T, ST>
+    where
+        D: Datable,
+        S: Spatial,
+        T: Temporal,
+        ST: SpaceTemporal
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.format(f)
+    }
+}
+
+impl<D, S, T, ST> Display for Context<D, S, T, ST>
+    where
+        D: Datable,
+        S: Spatial,
+        T: Temporal,
+        ST: SpaceTemporal
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.format(f)
     }
 }
