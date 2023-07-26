1b67bdd4e85b42e878eac293d30836cec0b6d82e
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-26 19:18:31 +0800

    Updated example make script.

--- scripts/example.sh
+++ scripts/example.sh
@@ -3,4 +3,36 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-command cargo run --example ctx
+echo ""
+echo "--------------------------------"
+echo "Select example to run: "
+echo "--------------------------------"
+echo ""
+
+select opt in  csm ctx smoking quit;
+do
+  case $opt in
+    csm)
+      echo "Selected example: CSM (Causal State Machine)"
+      command cargo run --release --example csm
+      break
+      ;;
+    ctx)
+      echo "Selected example: CTX (Context)"
+      command cargo run --release --example ctx
+      break
+      ;;
+    smoking)
+      echo "Selected example: SMOKING (Smoking)"
+      command cargo run --release --example smoking
+      break
+      ;;
+    quit)
+      echo "Exiting!"
+      exit 0
+      ;;
+    *)
+      echo "Invalid option $REPLY"
+      ;;
+  esac
+done
