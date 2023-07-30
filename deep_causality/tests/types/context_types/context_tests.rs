19b7fee24074d03b848d927983f6ace302e25e88
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-30 13:30:30 +0800

    Updated context type tests.

--- deep_causality/tests/types/context_types/context_tests.rs
+++ deep_causality/tests/types/context_types/context_tests.rs
@@ -1,6 +1,6 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
-use deep_causality::prelude::{Context, Contextoid, Contextuable, Dataoid, ContextoidType, Root, Spaceoid, SpaceTempoid, Tempoid, TimeScale};
+use deep_causality::prelude::{Context, Contextoid, Contextuable, Dataoid, ContextoidType, Root, Spaceoid, SpaceTempoid, Tempoid, TimeScale, RelationKind};
 
 fn get_context<'l>() -> Context<'l, Dataoid, Spaceoid, Tempoid, SpaceTempoid>
 {
@@ -156,7 +156,7 @@ fn test_add_edge() {
     let contextoid  = Contextoid::new(id, ContextoidType::Tempoid(tempoid));
     let t_idx = context.add_contextoid(&contextoid);
 
-    context.add_edge(roodidx, t_idx);
+    context.add_edge(roodidx, t_idx, RelationKind::Temporal);
 
     assert!(context.contains_edge(roodidx, t_idx));
 }
@@ -184,40 +184,10 @@ fn test_contains_edge() {
 
     let id = 2;
     let contextoid  = Contextoid::new(id, ContextoidType::Tempoid(tempoid));
-    let t_idx = context.add_contextoid(&contextoid);context.add_edge(roodidx, t_idx);
-
-    assert!(context.contains_edge(roodidx, t_idx));
-}
-
-#[test]
-fn test_add_edg_with_weight() {
-    let id = 1;
-    let mut context = get_context();
-    assert_eq!(context.size(), 0);
-
-    let root = Root::new(id);
-    let contextoid  = Contextoid::new(id, ContextoidType::Root(root));
-    let roodidx = context.add_contextoid(&contextoid);
-
-    assert_eq!(context.size(), 1);
-    assert!(context.contains_contextoid(roodidx));
-
-    let contextoid = context.get_contextoid(roodidx);
-    assert!(contextoid.is_some());
-
-    let t_id = 12;
-    let t_time_scale = TimeScale::Month;
-    let t_time_unit = 12;
-    let tempoid = Tempoid::new(t_id, t_time_scale, t_time_unit);
-
-    let id = 2;
-    let contextoid  = Contextoid::new(id, ContextoidType::Tempoid(tempoid));
     let t_idx = context.add_contextoid(&contextoid);
-
-    context.add_edg_with_weight(roodidx, t_idx, 42);
+    context.add_edge(roodidx, t_idx, RelationKind::Temporal);
 
     assert!(context.contains_edge(roodidx, t_idx));
-    assert_eq!(context.size(), 2);
 }
 
 #[test]
@@ -243,7 +213,8 @@ fn test_remove_edge() {
 
     let id = 2;
     let contextoid  = Contextoid::new(id, ContextoidType::Tempoid(tempoid));
-    let t_idx = context.add_contextoid(&contextoid);context.add_edge(roodidx, t_idx);
+    let t_idx = context.add_contextoid(&contextoid);
+    context.add_edge(roodidx, t_idx, RelationKind::Temporal);
 
     assert!(context.contains_edge(roodidx, t_idx));
 
