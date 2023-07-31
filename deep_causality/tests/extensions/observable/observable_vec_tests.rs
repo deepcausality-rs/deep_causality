2d40eab75ab57069f95023a82c0085015f8ea748
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-31 13:26:17 +0800

    Added missing tests to observable type extensions.

--- deep_causality/tests/extensions/observable/observable_vec_tests.rs
+++ deep_causality/tests/extensions/observable/observable_vec_tests.rs
@@ -34,6 +34,24 @@ fn test_percent_observation() {
 }
 
 #[test]
+fn test_number_non_observation() {
+    let observations = get_test_obs_vec();
+    let target_threshold = 10.0 as NumericalValue;
+    let target_effect = 1.0 as NumericalValue;
+    let total_non_observation = observations.number_non_observation(target_threshold, target_effect);
+    assert_eq!(2.0, total_non_observation);
+}
+
+#[test]
+fn test_percent_non_observation() {
+    let observations = get_test_obs_vec();
+    let target_threshold = 10.0;
+    let target_effect = 1.0;
+    let percent_non_observation = observations.percent_non_observation(target_threshold, target_effect);
+    assert_eq!(0.4, percent_non_observation);
+}
+
+#[test]
 fn test_get_all_items() {
     let observations = get_test_obs_vec();
     let all_items = observations.get_all_items();
