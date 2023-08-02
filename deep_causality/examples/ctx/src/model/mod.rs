706bcd9b6b34b3391d2a6ea6f85e17648f57d009
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-02 12:02:26 +0800

    Added helper function to return main causaloid for CTX model.

--- deep_causality/examples/ctx/src/model/mod.rs
+++ deep_causality/examples/ctx/src/model/mod.rs
@@ -1,4 +1,16 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
+
+use crate::model::causaloid_year::get_year_causaloid;
+use deep_causality::prelude::{Causaloid, Context, Spaceoid, SpaceTempoid, Tempoid};
+use crate::types::dateoid::Dataoid;
+
 pub mod utils;
 pub mod causaloid_year;
 
+pub fn get_causaloid<'l>(
+    context: &'l Context<'l, Dataoid, Spaceoid, Tempoid, SpaceTempoid>
+)
+    -> Causaloid<'l, Dataoid, Spaceoid, Tempoid, SpaceTempoid>
+{
+    get_year_causaloid(context)
+}
