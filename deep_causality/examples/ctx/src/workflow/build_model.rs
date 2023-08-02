dc54dd34ef8560b9b47bb8ab3699b0e4d24054ca
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-02 11:49:24 +0800

    Added build model function to  ctx example.

--- deep_causality/examples/ctx/src/workflow/build_model.rs
+++ deep_causality/examples/ctx/src/workflow/build_model.rs
@@ -1,3 +1,31 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
 
+use std::error::Error;
+use deep_causality::prelude::{Context, Model, Spaceoid, SpaceTempoid, Tempoid};
+use crate::model::causaloid_year::get_year_causaloid;
+use crate::types::dateoid::Dataoid;
+
+pub fn build_model<'l>(
+    context: &'l Context<Dataoid, Spaceoid, Tempoid, SpaceTempoid>
+)
+    -> Result<Model<'l, Dataoid, Spaceoid, Tempoid, SpaceTempoid>, Box<dyn Error>>
+{
+
+    let id = 1;
+    let author = "Marvin Hansen <marvin.hansen@gmail.com>";
+    let description = "This is a test model";
+    let assumptions = None;
+    let causaloid = get_year_causaloid(context);
+
+    Ok(
+        Model::new(
+            id,
+            author,
+            description,
+            assumptions,
+            &causaloid,
+            Some(context)
+        )
+    )
+}
