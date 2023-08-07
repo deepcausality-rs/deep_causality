c054b8e545e731fc222e381369a980d03d14efa7
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-07 12:01:44 +0800

    Renamed file

--- deep_causality/examples/ctx/src/workflow/mod.rs
+++ deep_causality/examples/ctx/src/workflow/mod.rs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 pub mod augment_data;
-pub mod gen_data_time_graph;
+pub mod build_context;
 pub mod load_data;
 pub mod build_model;
 
-pub use gen_data_time_graph::build_time_data_context;
+pub use build_context::build_time_data_context;
 pub use load_data::load_data;
