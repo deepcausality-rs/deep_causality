1d46bb9edca57de414879a877ba4e2a85ea1fd69
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-07 13:54:33 +0800

    Added utils to DTX example

---
+++ deep_causality/examples/dtx/src/utils.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
+
+use std::time::Duration;
+
+pub fn print_duration(msg: &str, elapsed: &Duration)
+{
+    println!("{} took: {:?} ", msg, elapsed);
+    println!();
+}
