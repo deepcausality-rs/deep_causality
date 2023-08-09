4bece1dd94dfb7ac988cfc8ca8ec54845b895b6e
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-09 11:43:44 +0800

    Added assumable type extension for BTreeMap, HashSet, and BTreeSet

--- deep_causality/src/extensions/assumable/mod.rs
+++ deep_causality/src/extensions/assumable/mod.rs
@@ -2,11 +2,12 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
 use deep_causality_macros::{make_get_all_items, make_get_all_map_items, make_is_empty, make_len};
-use std::collections::HashMap;
+use std::collections::{ BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
 use std::hash::Hash;
 use crate::prelude::{Assumable, AssumableReasoning};
 
 
+/// https://doc.rust-lang.org/std/primitive.array.html
 impl<T> AssumableReasoning<T> for [T]
     where
         T: Assumable + Clone,
@@ -16,7 +17,7 @@ impl<T> AssumableReasoning<T> for [T]
     make_get_all_items!();
 }
 
-
+/// https://doc.rust-lang.org/std/collections/hash_map/struct.HashMap.html
 impl<K, V> AssumableReasoning<V> for HashMap<K, V>
     where
         K: Eq + Hash,
@@ -27,8 +28,49 @@ impl<K, V> AssumableReasoning<V> for HashMap<K, V>
     make_get_all_map_items!();
 }
 
+/// https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
+impl<K, V> AssumableReasoning<V> for BTreeMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Assumable + Clone,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_map_items!();
+}
 
+/// https://doc.rust-lang.org/std/collections/hash_set/struct.HashSet.html
+impl<T> AssumableReasoning<T> for HashSet<T>
+    where
+        T: Assumable + Clone,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_items!();
+}
+
+/// https://doc.rust-lang.org/stable/std/collections/struct.BTreeSet.html
+impl<T> AssumableReasoning<T> for BTreeSet<T>
+    where
+        T: Assumable + Clone,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_items!();
+}
+
+/// https://doc.rust-lang.org/std/vec/struct.Vec.html
 impl<T> AssumableReasoning<T> for Vec<T>
+    where
+        T: Assumable + Clone,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_items!();
+}
+
+/// https://doc.rust-lang.org/std/collections/struct.VecDeque.html
+impl<T> AssumableReasoning<T> for VecDeque<T>
     where
         T: Assumable + Clone,
 {
