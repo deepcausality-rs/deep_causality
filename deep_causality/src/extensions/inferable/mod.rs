18bb0cae21dba4b2bb8d6cbae339ef81ef4a34de
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-09 12:47:04 +0800

    Added tests for inferable VecDeque.

--- deep_causality/src/extensions/inferable/mod.rs
+++ deep_causality/src/extensions/inferable/mod.rs
@@ -2,10 +2,39 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
 use deep_causality_macros::{make_get_all_items, make_get_all_map_items, make_is_empty, make_len};
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap, VecDeque};
 use std::hash::Hash;
 use crate::prelude::{Inferable, InferableReasoning};
 
+
+impl<T> InferableReasoning<T> for [T]
+    where
+        T: Inferable,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_items!();
+}
+
+impl<K, V> InferableReasoning<V> for HashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Inferable,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_map_items!();
+}
+
+impl<K, V> InferableReasoning<V> for BTreeMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Inferable,
+{
+    make_len!();
+    make_is_empty!();
+    make_get_all_map_items!();
+}
 
 impl<T> InferableReasoning<T> for Vec<T>
     where
@@ -16,19 +45,7 @@ impl<T> InferableReasoning<T> for Vec<T>
     make_get_all_items!();
 }
 
-
-impl<K, V> InferableReasoning<V> for HashMap<K, V>
-    where
-        K: Eq + Hash,
-        V: Inferable + Clone,
-{
-    make_len!();
-    make_is_empty!();
-    make_get_all_map_items!();
-}
-
-
-impl<T> InferableReasoning<T> for [T]
+impl<T> InferableReasoning<T> for VecDeque<T>
     where
         T: Inferable,
 {
---
+++ deep_causality/tests/extensions/inferable/inferable_btree_map_tests.rs
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
+
+use std::collections::{BTreeMap};
+use deep_causality::prelude::*;
+use deep_causality::utils::test_utils::{get_test_inferable};
+
+fn get_test_inf_btree_map()
+    -> BTreeMap<usize, Inference>
+{
+    let i1 = get_test_inferable(0, true);
+    let i2 = get_test_inferable(1, false);
+    BTreeMap::from_iter([(1, i1), (2, i2)])
+}
+
+#[test]
+fn test_add() {
+    let mut col = get_test_inf_btree_map();
+    assert_eq!(2, col.len());
+
+    let f3 = get_test_inferable(3, true);
+    col.insert(3,f3);
+    assert_eq!(3, col.len());
+}
+
+#[test]
+fn test_all_inferable() {
+    let f = get_test_inferable(3, false);
+    let col: Vec<Inference> = Vec::from_iter([f]);
+    assert!(col.all_inferable());
+}
+
+#[test]
+fn test_all_inverse_inferable() {
+    let f = get_test_inferable(3, true);
+    let col: Vec<Inference> = Vec::from_iter([f]);
+    assert!(col.all_inverse_inferable());
+}
+
+#[test]
+fn test_all_non_inferable() {
+    let col = get_test_inf_btree_map();
+    assert!(!col.all_non_inferable());
+}
+
+#[test]
+fn test_conjoint_delta() {
+    let col = get_test_inf_btree_map();
+    // in the synthetic test data,
+    // the conjoint delta is 0.0% because all causes explain the observed effects.
+    assert_eq!(0.0, col.conjoint_delta());
+}
+
+#[test]
+fn test_number_inferable() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(1.0, col.number_inferable());
+}
+
+#[test]
+fn test_number_inverse_inferable() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(1.0, col.number_inverse_inferable());
+}
+
+#[test]
+fn test_number_non_inferable() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(0.0, col.number_non_inferable());
+}
+
+#[test]
+fn test_percent_inferable() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(50.0, col.percent_inferable())
+}
+
+#[test]
+fn test_percent_inverse_inferable() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(50.0, col.percent_inverse_inferable())
+}
+
+#[test]
+fn test_percent_non_inferable() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(0.0, col.percent_non_inferable())
+}
+
+#[test]
+fn test_get_all_inferable() {
+    let mut col = get_test_inf_btree_map();
+    let f3 = get_test_inferable(3, false);
+    col.insert(3, f3);
+    let all_inf = col.get_all_inferable();
+    assert_eq!(2, all_inf.len());
+}
+
+#[test]
+fn test_get_all_inverse_inferable() {
+    let col = get_test_inf_btree_map();
+    let all_inv_inf = col.get_all_inverse_inferable();
+    assert_eq!(1, all_inv_inf.len());
+}
+
+#[test]
+fn test_get_all_non_inferable() {
+    let col = get_test_inf_btree_map();
+    let all_non_inf = col.get_all_non_inferable();
+    assert_eq!(0, all_non_inf.len());
+}
+
+#[test]
+fn test_get_all_items() {
+    let col = get_test_inf_btree_map();
+    let all_items = col.get_all_items();
+
+    let exp_len = col.len();
+    let actual_len = all_items.len();
+    assert_eq!(exp_len, actual_len);
+}
+
+#[test]
+fn test_len() {
+    let col = get_test_inf_btree_map();
+    assert_eq!(2, col.len());
+}
+
+#[test]
+fn test_is_empty() {
+    let col = get_test_inf_btree_map();
+    assert!(!col.is_empty());
+}
