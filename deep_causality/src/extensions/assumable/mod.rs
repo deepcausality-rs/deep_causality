278b199a2638a01aabbc7bba69c3e8f7aba56b4c
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-09 13:51:23 +0800

    Added documentation to type extensions.

--- deep_causality/src/extensions/assumable/mod.rs
+++ deep_causality/src/extensions/assumable/mod.rs
@@ -2,12 +2,11 @@
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
 use deep_causality_macros::{make_get_all_items, make_get_all_map_items, make_is_empty, make_len};
-use std::collections::{ BTreeMap, HashMap, VecDeque};
+use std::collections::{BTreeMap, HashMap, VecDeque};
 use std::hash::Hash;
 use crate::prelude::{Assumable, AssumableReasoning};
 
 
-/// https://doc.rust-lang.org/std/primitive.array.html
 impl<T> AssumableReasoning<T> for [T]
     where
         T: Assumable,
@@ -17,7 +16,6 @@ impl<T> AssumableReasoning<T> for [T]
     make_get_all_items!();
 }
 
-/// https://doc.rust-lang.org/std/collections/hash_map/struct.HashMap.html
 impl<K, V> AssumableReasoning<V> for HashMap<K, V>
     where
         K: Eq + Hash,
@@ -28,7 +26,6 @@ impl<K, V> AssumableReasoning<V> for HashMap<K, V>
     make_get_all_map_items!();
 }
 
-/// https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
 impl<K, V> AssumableReasoning<V> for BTreeMap<K, V>
     where
         K: Eq + Hash,
@@ -39,7 +36,6 @@ impl<K, V> AssumableReasoning<V> for BTreeMap<K, V>
     make_get_all_map_items!();
 }
 
-/// https://doc.rust-lang.org/std/vec/struct.Vec.html
 impl<T> AssumableReasoning<T> for Vec<T>
     where
         T: Assumable,
@@ -49,7 +45,6 @@ impl<T> AssumableReasoning<T> for Vec<T>
     make_get_all_items!();
 }
 
-/// https://doc.rust-lang.org/std/collections/struct.VecDeque.html
 impl<T> AssumableReasoning<T> for VecDeque<T>
     where
         T: Assumable,
