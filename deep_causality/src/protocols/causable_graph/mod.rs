e2b3f81fadf6cf5d4106dcb7cdc1b43c78a6cbcf
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-10 13:33:42 +0800

    Moved type aliases into causable graph protocol and made them public

--- deep_causality/src/protocols/causable_graph/mod.rs
+++ deep_causality/src/protocols/causable_graph/mod.rs
@@ -1,5 +1,19 @@
+use petgraph::graph::NodeIndex as GraphNodeIndex;
+use std::collections::HashMap;
+use petgraph::matrix_graph::MatrixGraph;
+use petgraph::Directed;
+
 pub mod causable_graph_type;
 pub mod causable_graph_reasoning;
+pub mod causable_graph_explaining;
 
-pub use causable_graph_type::CausableGraph;
-pub use crate::prelude::Causable;
+// Custom index type. See documentation in
+// src/protocols/contextuable/csm_types
+// for more details.
+pub type DefaultIx = u32;
+pub type NodeIndex<Ix = DefaultIx> = GraphNodeIndex<Ix>;
+pub type IndexMap = HashMap<usize, NodeIndex>;
+
+// CausalGraph type alias
+// Edge weights need to be numerical (u64) to make shortest path algo work.
+pub type CausalGraph<T> = MatrixGraph<T, u64, Directed, Option<u64>, u32>;
