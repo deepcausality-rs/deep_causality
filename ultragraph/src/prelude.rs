9d2b7082e6041bae3183626b6c8f6d9872ad6d86
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-17 13:10:55 +0800

    Removed dead code in ultragraph
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- ultragraph/src/prelude.rs
+++ ultragraph/src/prelude.rs
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
 
+// Alias type renames the container type to UltraGraph
+pub use crate::alias::UltraGraph;
 // Errors
 pub use crate::errors::UltraGraphError;
+// Protocols
 pub use crate::protocols::graph_like::GraphLike;
 pub use crate::protocols::graph_root::GraphRoot;
 pub use crate::protocols::graph_storage::GraphStorage;
-// Protocols
-pub use crate::protocols::UltraGraphable;
-// Storage
+// Storage implementation
 pub use crate::storage::matrix_graph::UltraMatrixGraph;
 // Main type
 pub use crate::types::ultra_graph::UltraGraphContainer;
