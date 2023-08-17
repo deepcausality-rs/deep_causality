276f234976909a82959880a5e8720592e4f39a9e
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-17 11:20:44 +0800

    Added additional constructors to ultragraph.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- ultragraph/src/lib.rs
+++ ultragraph/src/lib.rs
@@ -12,9 +12,7 @@ pub mod storage;
 pub mod types;
 
 /// Returns a new UltraGraph with matrix storage backend.
-///
-/// # Arguments
-/// * Capacity refers to the maximum number of nodes that fit into the graph before a resize occurs.
+/// Default capacity is 500 nodes.
 ///
 /// # Example:
 /// ```
@@ -25,7 +23,7 @@ pub mod types;
 ///     x: u8,
 /// }
 ///
-///  let mut g = ultragraph::new_with_matrix_storage::<Data>(10);
+///  let mut g = ultragraph::new::<Data>();
 ///  assert!(g.is_empty());
 ///
 ///  let d = Data { x: 1 };
@@ -42,6 +40,52 @@ pub mod types;
 ///  let res = g.add_edge(root_index, node_a_index);
 ///  assert!(res.is_ok());
 /// ```
+pub fn new<T>()
+    -> UltraGraph<StorageMatrixGraph<T>, T>
+{
+    UltraGraph::new(StorageMatrixGraph::<T>::new_with_capacity(500))
+}
+
+
+/// Returns a new UltraGraph with matrix storage backend.
+///
+/// # Arguments
+/// * Capacity refers to the maximum number of nodes that fit into the graph before a resize occurs.
+///
+/// # Example:
+/// ```
+/// use ultragraph::prelude::*;
+///
+/// #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+/// pub struct Data {
+///     x: u8,
+/// }
+///
+///  let mut g = ultragraph::with_capacity::<Data>(10);
+///  assert!(g.is_empty());
+///
+///  let d = Data { x: 1 };
+///  let root_index = g.add_root_node(d);
+///  assert_eq!(root_index, 0);
+///
+///  let d = Data { x: 42 };
+///  let node_a_index = g.add_node(d);
+///  assert_eq!(node_a_index, 1);
+///
+///  let data = g.get_node(1).unwrap();
+///  assert_eq!(data.x, d.x);
+///
+///  let res = g.add_edge(root_index, node_a_index);
+///  assert!(res.is_ok());
+/// ```
+pub fn with_capacity<T>(
+    capacity: usize
+)
+    -> UltraGraph<StorageMatrixGraph<T>, T>
+{
+    UltraGraph::new(StorageMatrixGraph::<T>::new_with_capacity(capacity))
+}
+
 pub fn new_with_matrix_storage<T>(
     capacity: usize
 )
