49416304297cafbdb0f3cd494ae292ae45cf35f6
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-17 13:58:41 +0800

    Added benchmarks to ultragraph
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ ultragraph/benches/benchmarks/data.rs
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) "2023" . Marvin Hansen <marvin.hansen@gmail.com> All rights reserved.
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub struct Data {
+    pub a: u8,
+    pub b: u8,
+    pub z: bool,
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        Self {
+            a: 0,
+            b: 1,
+            z: false,
+        }
+    }
+}
