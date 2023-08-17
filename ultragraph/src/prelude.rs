6c2c78510d1efdea951378b7330d2bececf2cefa
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-17 13:02:28 +0800

    Updated ultragraph  prelude
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- ultragraph/src/prelude.rs
+++ ultragraph/src/prelude.rs
@@ -9,7 +9,7 @@ pub use crate::protocols::graph_storage::GraphStorage;
 // Protocols
 pub use crate::protocols::UltraGraphable;
 // Storage
-pub use crate::storage::storage_matrix::StorageMatrixGraph;
+pub use crate::storage::matrix_graph::UltraMatrixGraph;
 // Main type
-pub use crate::types::ultra_graph::UltraGraph;
+pub use crate::types::ultra_graph::UltraGraphContainer;
 
