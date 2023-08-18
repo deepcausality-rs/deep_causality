f1604eb7edde2540e0a99403d6c419dc584f6b70
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-18 17:43:56 +0800

    Added more tests to CSM types.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality/tests/types/csm_types/csm_state_tests.rs
+++ deep_causality/tests/types/csm_types/csm_state_tests.rs
@@ -80,3 +80,22 @@ fn eval_with_data()
     let trigger = res.expect("Failed to unwrap eval result from causal state");
     assert!(trigger);
 }
+
+#[test]
+fn test_to_string()
+{
+    let causaloid = &test_utils::get_test_causaloid();
+    assert!(causaloid.is_singleton());
+    assert_eq!(01, causaloid.id());
+    assert_eq!("tests whether data exceeds threshold of 0.55".to_string(), causaloid.description());
+    assert!(!causaloid.is_active());
+
+    let id = 42;
+    let version = 1;
+    let data = 0.23f64;
+    let cs = CausalState::new(id, version, data, causaloid);
+
+    let expected = "CausalState: \n id: 42 version: 1 \n data: 0.23 causaloid: Causaloid id: 1 \n Causaloid type: Singleton \n description: tests whether data exceeds threshold of 0.55 is active: false has context: false".to_string();
+    let actual = cs.to_string();
+    assert_eq!(actual, expected)
+}
