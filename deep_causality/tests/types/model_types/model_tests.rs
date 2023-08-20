73a7b3110f849e570fb4b0e712156f997759cfab
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-20 18:19:55 +0800

    Update tests to match generated getters.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality/tests/types/model_types/model_tests.rs
+++ deep_causality/tests/types/model_types/model_tests.rs
@@ -2,7 +2,8 @@
 // Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
 
 
-use deep_causality::prelude::{Identifiable, Model};
+use deep_causality::prelude::Identifiable;
+use deep_causality::types::model_types::Model;
 
 use crate::utils::test_utils::*;
 
@@ -34,6 +35,7 @@ fn test_id() {
     let model = Model::new(id, author, description, assumptions, causaloid, context);
 
     assert_eq!(model.id(), id);
+    assert_eq!(*model.model_id(), id);
 }
 
 #[test]
@@ -49,7 +51,7 @@ fn test_author() {
     let model = Model::new(id, author, description, assumptions, causaloid, context);
 
     assert_eq!(model.id(), id);
-    assert_eq!(model.author(), author);
+    assert_eq!(*model.author(), author);
 }
 
 #[test]
@@ -65,8 +67,8 @@ fn test_description() {
     let model = Model::new(id, author, description, assumptions, causaloid, context);
 
     assert_eq!(model.id(), id);
-    assert_eq!(model.author(), author);
-    assert_eq!(model.description(), description);
+    assert_eq!(*model.author(), author);
+    assert_eq!(*model.description(), description);
 }
 
 #[test]
@@ -82,8 +84,8 @@ fn test_assumptions() {
     let model = Model::new(id, author, description, assumptions, causaloid, context);
 
     assert_eq!(model.id(), id);
-    assert_eq!(model.author(), author);
-    assert_eq!(model.description(), description);
+    assert_eq!(*model.author(), author);
+    assert_eq!(*model.description(), description);
     assert!(model.assumptions().is_none());
 }
 
@@ -100,10 +102,10 @@ fn test_causaloid() {
     let model = Model::new(id, author, description, assumptions, causaloid, context);
 
     assert_eq!(model.id(), id);
-    assert_eq!(model.author(), author);
-    assert_eq!(model.description(), description);
+    assert_eq!(*model.author(), author);
+    assert_eq!(*model.description(), description);
     assert!(model.assumptions().is_none());
-    assert_eq!(model.causaloid(), causaloid);
+    assert_eq!(*model.causaloid(), causaloid);
 }
 
 #[test]
@@ -119,10 +121,10 @@ fn test_context() {
     let model = Model::new(id, author, description, assumptions, causaloid, context);
 
     assert_eq!(model.id(), id);
-    assert_eq!(model.author(), author);
-    assert_eq!(model.description(), description);
+    assert_eq!(*model.author(), author);
+    assert_eq!(*model.description(), description);
     assert!(model.assumptions().is_none());
-    assert_eq!(model.causaloid(), causaloid);
+    assert_eq!(*model.causaloid(), causaloid);
     assert!(model.context().is_some());
     assert_eq!(model.context().unwrap().id(), context.unwrap().id());
 }
