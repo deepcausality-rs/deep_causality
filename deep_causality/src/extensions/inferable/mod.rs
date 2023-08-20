655fa652dbe2276c15ce51470ccb9be6bf7048dc
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-20 20:37:37 +0800

    Code formatting.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality/src/extensions/inferable/mod.rs
+++ deep_causality/src/extensions/inferable/mod.rs
@@ -12,9 +12,7 @@ impl<T> InferableReasoning<T> for [T]
     where
         T: Inferable,
 {
-    make_len!();
-    make_is_empty!();
-    make_get_all_items!();
+    make_len!();make_is_empty!();make_get_all_items!();
 }
 
 impl<K, V> InferableReasoning<V> for HashMap<K, V>
@@ -22,9 +20,7 @@ impl<K, V> InferableReasoning<V> for HashMap<K, V>
         K: Eq + Hash,
         V: Inferable,
 {
-    make_len!();
-    make_is_empty!();
-    make_get_all_map_items!();
+    make_len!();make_is_empty!();make_get_all_map_items!();
 }
 
 impl<K, V> InferableReasoning<V> for BTreeMap<K, V>
@@ -32,25 +28,19 @@ impl<K, V> InferableReasoning<V> for BTreeMap<K, V>
         K: Eq + Hash,
         V: Inferable,
 {
-    make_len!();
-    make_is_empty!();
-    make_get_all_map_items!();
+    make_len!();make_is_empty!();make_get_all_map_items!();
 }
 
 impl<T> InferableReasoning<T> for Vec<T>
     where
         T: Inferable,
 {
-    make_len!();
-    make_is_empty!();
-    make_get_all_items!();
+    make_len!();make_is_empty!();make_get_all_items!();
 }
 
 impl<T> InferableReasoning<T> for VecDeque<T>
     where
         T: Inferable,
 {
-    make_len!();
-    make_is_empty!();
-    make_get_all_items!();
+    make_len!();make_is_empty!();make_get_all_items!();
 }
