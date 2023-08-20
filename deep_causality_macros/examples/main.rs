61a7ed9cd75ef6a8cf580ae57d00368387823ed6
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-20 17:31:04 +0800

    Added example code for new macros.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ deep_causality_macros/examples/main.rs
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
+
+use deep_causality_macros::{Constructor, Getters};
+
+#[derive(Getters, Constructor, Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub struct Data<T> {
+    #[getter(name = data_id)] // Rename getter methods as you wish
+    id: u64,
+    data: T,
+    filled: bool,
+}
+
+pub fn main() {
+    let d = Data::new(0, 42, true);
+
+    assert_eq!(*d.data_id(), 0);
+    assert_eq!(*d.data(), 42);
+    assert_eq!(*d.filled(), true);
+}
