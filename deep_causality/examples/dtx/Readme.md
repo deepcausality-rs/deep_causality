6cbe57da9fb06b1fce015352a2e2a53b988cf251
Marvin Hansen <marvin.hansen@gmail.com>
2023-08-25 00:54:58 +0800

    Removed dead code from an unfinished example
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality/examples/dtx/Readme.md
+++
@@ -1,4 +0,0 @@
-# Work in progress
-
-This example is currently excluded from the
-global workspace in Cargo.toml to avoid breaking CI runs. 
--- deep_causality/examples/dtx/src/types/mod.rs
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
-pub mod config_types;
-pub mod data_types;
