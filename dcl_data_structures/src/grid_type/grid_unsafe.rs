c1c702931ae725b81c3e892349f8ebce8f600e78
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-22 13:30:36 +0800

    Added unsafe ArrayGrid Type that performs up tp 50% faster than the safe counter part
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ dcl_data_structures/src/grid_type/grid_unsafe.rs
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
+
+use std::fmt::Debug;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+use crate::prelude::{PointIndex, Storage};
+
+// A Grid API, with four different implementations backed by const generic arrays.
+// https://github.com/adamchalmers/const_generic_grid
+#[derive(Debug)]
+pub struct Grid<S, T>
+where
+    T: Copy,
+    S: Storage<T>,
+{
+    storage: S,
+    initialized: AtomicBool,
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<S, T> Grid<S, T>
+where
+    T: Copy + Default,
+    S: Storage<T>,
+{
+    #[inline(always)]
+    pub fn new(storage: S) -> Self {
+        Self {
+            storage,
+            initialized: AtomicBool::new(true),
+            _marker: std::marker::PhantomData,
+        }
+    }
+
+    #[inline(always)]
+    pub fn get(&self, p: PointIndex) -> T {
+        if self.initialized.load(Ordering::Relaxed) {
+            *self.storage.get(p)
+        } else {
+            T::default()
+        }
+    }
+
+    #[inline(always)]
+    pub fn set(&self, p: PointIndex, value: T) {
+        // SAFETY: This is safe because we're using atomic operations to ensure thread safety
+        // and we know the storage is properly initialized
+        unsafe {
+            let storage_ptr = &self.storage as *const S as *mut S;
+            (*storage_ptr).set(p, value);
+        }
+    }
+
+    #[inline(always)]
+    pub fn depth(&self) -> Option<usize> {
+        self.storage.depth().copied()
+    }
+
+    #[inline(always)]
+    pub fn height(&self) -> Option<usize> {
+        self.storage.height().copied()
+    }
+
+    #[inline(always)]
+    pub fn time(&self) -> Option<usize> {
+        self.storage.time().copied()
+    }
+
+    #[inline(always)]
+    pub fn width(&self) -> Option<usize> {
+        self.storage.width().copied()
+    }
+}
--- dcl_data_structures/src/grid_type/mod.rs
+++ dcl_data_structures/src/grid_type/mod.rs
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
 
-#![forbid(unsafe_code)]
-
-use std::fmt::{Debug, Display, Formatter};
-
-use crate::prelude::{Grid, PointIndex};
-
 pub mod grid;
+#[cfg(not(feature = "unsafe"))]
+pub mod grid_safe;
+#[cfg(feature = "unsafe")]
+pub mod grid_unsafe;
 pub mod point;
 pub mod storage;
 pub mod storage_array_1d;
@@ -15,15 +13,11 @@ pub mod storage_array_2d;
 pub mod storage_array_3d;
 pub mod storage_array_4d;
 
-/// Type alias for fixed sized static ArrayGrid
-pub type ArrayGrid1DType<T, const H: usize> = Grid<[T; H], T>;
-pub type ArrayGrid2DType<T, const W: usize, const H: usize> = Grid<[[T; W]; H], T>;
-pub type ArrayGrid3DType<T, const W: usize, const H: usize, const D: usize> =
-    Grid<[[[T; W]; H]; D], T>;
-pub type ArrayGrid4DType<T, const W: usize, const H: usize, const D: usize, const C: usize> =
-    Grid<[[[[T; W]; H]; D]; C], T>;
+use std::fmt::{Debug, Display, Formatter};
 
-/// ArrayType to determine what kind of ArrayGrid to build.
+use crate::prelude::{Grid, PointIndex};
+
+#[derive(Debug, Copy, Clone)]
 pub enum ArrayType {
     Array1D,
     Array2D,
@@ -31,58 +25,25 @@ pub enum ArrayType {
     Array4D,
 }
 
+// ArrayGrid is a wrapper around Grid that provides a more convenient API
+// for working with arrays of different dimensions.
+#[derive(Debug)]
 pub enum ArrayGrid<T, const W: usize, const H: usize, const D: usize, const C: usize>
 where
-    T: Copy,
+    T: Copy + Default,
 {
-    ArrayGrid1D(ArrayGrid1DType<T, H>),
-    ArrayGrid2D(ArrayGrid2DType<T, W, H>),
-    ArrayGrid3D(ArrayGrid3DType<T, W, H, D>),
-    ArrayGrid4D(ArrayGrid4DType<T, W, H, D, C>),
+    ArrayGrid1D(Grid<[T; H], T>),
+    ArrayGrid2D(Grid<[[T; W]; H], T>),
+    ArrayGrid3D(Grid<[[[T; W]; H]; D], T>),
+    ArrayGrid4D(Grid<[[[[T; W]; H]; D]; C], T>),
 }
 
 impl<T, const W: usize, const H: usize, const D: usize, const C: usize> ArrayGrid<T, W, H, D, C>
 where
     T: Copy + Default,
 {
+    #[inline(always)]
     /// Returns a new ArrayGrid with the ArrayType given as argument
-    ///
-    /// # Const Generic Arguments
-    /// * T - Type to store i.e. u64 or MyStruct
-    /// * W: usize - Width (First ArrayGrid dimension)
-    /// * H: usize - Height (Second ArrayGrid dimension)
-    /// * D: usize - Depth (Third ArrayGrid dimension)
-    /// * C: usize - Chronos (Time) since T was already taken for Type T (Fourth ArrayGrid dimension)
-    ///
-    /// Note, if you only use up to, say, 2D just set Depth and Time to 1.
-    ///
-    /// # Arguments
-    ///
-    /// * `array_type: ArrayType` - Enum that defines which type of ArrayGrid to build.
-    ///
-    /// # Example
-    ///
-    /// In most cases, the sample code below should suffice. You do need type annotation
-    /// because of the const generic arguments.
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid};
-    ///
-    /// // Constants required for const generic parameters
-    /// // Use these to check whether your PointIndex stays within the Array boundaries.
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 5;
-    /// const TIME: usize = 1; // set to 1 if unused.
-    ///
-    ///     // Make a simple 1D Array of type usize
-    ///     let array_type = ArrayType::Array1D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///     // Make a 3D Array of type usize
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(ArrayType::Array3D);
-    ///
-    ///
-    /// ```
     pub fn new(array_type: ArrayType) -> ArrayGrid<T, W, H, D, C> {
         match array_type {
             ArrayType::Array1D => ArrayGrid::ArrayGrid1D(Grid::new([T::default(); H])),
@@ -94,32 +55,43 @@ where
         }
     }
 
-    /// Returns the item for the given PointIndex
-    ///
-    /// # Arguments
-    ///
-    /// * `p: PointIndex` - PointIndex that must match the dimensionality of the ArrayGrid
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid, PointIndex};
-    ///
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 1;  // set to 1 if unused.
-    /// const TIME: usize = 1;   // set to 1 if unused.
-    ///
-    ///     let array_type = ArrayType::Array1D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///     // Create a 1D PointIndex
-    ///     let p = PointIndex::new1d(1);
-    ///
-    ///     // Get the value for the point index
-    ///     let res = ag.get(p);
-    ///
-    /// ```
+    #[inline(always)]
+    pub fn array_grid_1d(&self) -> Option<&Grid<[T; H], T>> {
+        if let ArrayGrid::ArrayGrid1D(grid) = self {
+            Some(grid)
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn array_grid_2d(&self) -> Option<&Grid<[[T; W]; H], T>> {
+        if let ArrayGrid::ArrayGrid2D(grid) = self {
+            Some(grid)
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn array_grid_3d(&self) -> Option<&Grid<[[[T; W]; H]; D], T>> {
+        if let ArrayGrid::ArrayGrid3D(grid) = self {
+            Some(grid)
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn array_grid_4d(&self) -> Option<&Grid<[[[[T; W]; H]; D]; C], T>> {
+        if let ArrayGrid::ArrayGrid4D(grid) = self {
+            Some(grid)
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
     pub fn get(&self, p: PointIndex) -> T {
         match self {
             ArrayGrid::ArrayGrid1D(grid) => grid.get(p),
@@ -129,33 +101,7 @@ where
         }
     }
 
-    /// Sets the item at the given PointIndex
-    ///
-    /// # Arguments
-    ///
-    /// * `p: PointIndex` - PointIndex that must match the dimensionality of the ArrayGrid
-    /// * `value: T` -  Value to store at the index position.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid, PointIndex};
-    ///
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 1;  // set to 1 if unused.
-    /// const TIME: usize = 1;   // set to 1 if unused.
-    ///
-    ///     let array_type = ArrayType::Array1D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///      // Create a 1D PointIndex
-    ///      let p = PointIndex::new1d(1);
-    ///
-    ///      // Store a usize with the point index
-    ///      ag.set(p, 42);
-    ///
-    /// ```
+    #[inline(always)]
     pub fn set(&self, p: PointIndex, value: T) {
         match self {
             ArrayGrid::ArrayGrid1D(grid) => grid.set(p, value),
@@ -164,185 +110,19 @@ where
             ArrayGrid::ArrayGrid4D(grid) => grid.set(p, value),
         }
     }
-
-    /// Returns a 1D GridArray if the instance is 1D, otherwise returns None.
-    /// This give access to the array dimensions available in the underlying Grid Type.
-    ///
-    /// # Returns
-    ///  * Option<&ArrayGrid1DType>
-    ///
-    /// Note, ArrayGrid1DType is a type alias to Grid.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid, PointIndex};
-    ///
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 1;  // set to 1 if unused.
-    /// const TIME: usize = 1;   // set to 1 if unused.
-    ///
-    ///     let array_type = ArrayType::Array1D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///     let g = ag.array_grid_1d()
-    ///         .expect("failed to get 1D array grid");
-    ///
-    ///     // Access dimensions i.e. for index bound checks before insert.
-    ///     let height = g.height().unwrap();
-    ///     assert_eq!(height, HEIGHT);
-    ///
-    /// ```
-    pub fn array_grid_1d(&self) -> Option<&ArrayGrid1DType<T, H>> {
-        if let ArrayGrid::ArrayGrid1D(array_grid) = self {
-            Some(array_grid)
-        } else {
-            None
-        }
-    }
-
-    /// Returns a 2D GridArray if the instance is 3D, otherwise returns None.
-    /// This give access to the array dimensions available in the underlying Grid Type.
-    ///
-    /// # Returns
-    ///  * Option<&ArrayGrid2DType>
-    ///
-    /// Note, ArrayGrid2DType is a type alias to Grid.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid, PointIndex};
-    ///
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 1;  // set to 1 if unused.
-    /// const TIME: usize = 1;   // set to 1 if unused.
-    ///
-    ///     let array_type = ArrayType::Array2D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///     let g = ag.array_grid_2d()
-    ///         .expect("failed to get 2D array grid");
-    ///
-    ///     // Access dimensions i.e. for index bound checks before insert.
-    ///     let height = g.height().unwrap();
-    ///     assert_eq!(height, HEIGHT);
-    ///
-    ///     let width = g.width().unwrap();
-    ///     assert_eq!(width, WIDTH);
-    ///
-    /// ```
-    pub fn array_grid_2d(&self) -> Option<&ArrayGrid2DType<T, W, H>> {
-        if let ArrayGrid::ArrayGrid2D(array_grid) = self {
-            Some(array_grid)
-        } else {
-            None
-        }
-    }
-
-    /// Returns a 3D GridArray if the instance is 3D, otherwise returns None.
-    /// This give access to the array dimensions available in the underlying Grid Type.
-    ///
-    /// # Returns
-    ///  * Option<&ArrayGrid3DType>
-    ///
-    /// Note, ArrayGrid3DType is a type alias to Grid.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid, PointIndex};
-    ///
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 5;
-    /// const TIME: usize = 1;   // set to 1 if unused.
-    ///
-    ///     let array_type = ArrayType::Array3D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///     let g = ag.array_grid_3d()
-    ///         .expect("failed to get 3D array grid");
-    ///
-    ///     // Access dimensions i.e. for index bound checks before insert.
-    ///     let height = g.height().unwrap();
-    ///     assert_eq!(height, HEIGHT);
-    ///
-    ///     let width = g.width().unwrap();
-    ///     assert_eq!(width, WIDTH);
-    ///
-    ///     let depth = g.depth().unwrap();
-    ///     assert_eq!(depth, DEPTH);
-    ///
-    /// ```
-    pub fn array_grid_3d(&self) -> Option<&ArrayGrid3DType<T, W, H, D>> {
-        if let ArrayGrid::ArrayGrid3D(array_grid) = self {
-            Some(array_grid)
-        } else {
-            None
-        }
-    }
-
-    /// Returns a 4D GridArray if the instance is 4D, otherwise returns None.
-    /// This give access to the array dimensions available in the underlying Grid Type.
-    ///
-    /// # Returns
-    ///  * Option<&ArrayGrid4DType>
-    ///
-    /// Note, ArrayGrid4DType is a type alias to Grid.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use dcl_data_structures::prelude::{ArrayGrid, ArrayType, Grid, PointIndex};
-    ///
-    /// const WIDTH: usize = 5;
-    /// const HEIGHT: usize = 5;
-    /// const DEPTH: usize = 5;
-    /// const TIME: usize = 5;
-    ///
-    ///     let array_type = ArrayType::Array4D;
-    ///     let ag: ArrayGrid<usize, WIDTH, HEIGHT, DEPTH, TIME> = ArrayGrid::new(array_type);
-    ///
-    ///     let g = ag.array_grid_4d()
-    ///         .expect("failed to get 4D array grid");
-    ///
-    ///     // Access dimensions i.e. for index bound checks before insert.
-    ///     let height = g.height().unwrap();
-    ///     assert_eq!(height, HEIGHT);
-    ///
-    ///     let width = g.width().unwrap();
-    ///     assert_eq!(width, WIDTH);
-    ///
-    ///     let depth = g.depth().unwrap();
-    ///     assert_eq!(depth, DEPTH);
-    ///
-    ///    let time = g.time().unwrap();
-    ///     assert_eq!(time, TIME);
-    ///
-    /// ```
-    pub fn array_grid_4d(&self) -> Option<&ArrayGrid4DType<T, W, H, D, C>> {
-        if let ArrayGrid::ArrayGrid4D(array_grid) = self {
-            Some(array_grid)
-        } else {
-            None
-        }
-    }
 }
 
 impl<T, const W: usize, const H: usize, const D: usize, const C: usize> Display
     for ArrayGrid<T, W, H, D, C>
 where
-    T: Copy + Debug,
+    T: Copy + Default + Debug,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            ArrayGrid::ArrayGrid1D(array_grid) => write!(f, "ArrayGrid1D: {:?}", array_grid),
-            ArrayGrid::ArrayGrid2D(array_grid) => write!(f, "ArrayGrid2D: {:?}", array_grid),
-            ArrayGrid::ArrayGrid3D(array_grid) => write!(f, "ArrayGrid3D: {:?}", array_grid),
-            ArrayGrid::ArrayGrid4D(array_grid) => write!(f, "ArrayGrid4D: {:?}", array_grid),
+            ArrayGrid::ArrayGrid1D(_) => write!(f, "ArrayGrid1D"),
+            ArrayGrid::ArrayGrid2D(_) => write!(f, "ArrayGrid2D"),
+            ArrayGrid::ArrayGrid3D(_) => write!(f, "ArrayGrid3D"),
+            ArrayGrid::ArrayGrid4D(_) => write!(f, "ArrayGrid4D"),
         }
     }
 }
