76ab685ea93ba50e80e4f58b404dd42ee099c078
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-22 20:51:00 +0800

    Added tests for the custom rin_buffer
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ dcl_data_structures/tests/ring_buffer/utils/cursor_sequence_test.rs
@@ -0,0 +1,44 @@
+use dcl_data_structures::ring_buffer::sequence::sequence::AtomicSequence;
+use dcl_data_structures::ring_buffer::utils::cursor_sequence::min_cursor_sequence;
+
+#[test]
+fn test_min_cursor_sequence_empty() {
+    let sequences: Vec<AtomicSequence> = vec![];
+    assert_eq!(min_cursor_sequence(&sequences), 0);
+}
+
+#[test]
+fn test_min_cursor_sequence_single() {
+    let sequences = vec![AtomicSequence::from(42)];
+    assert_eq!(min_cursor_sequence(&sequences), 42);
+}
+
+#[test]
+fn test_min_cursor_sequence_multiple() {
+    let sequences = vec![
+        AtomicSequence::from(10),
+        AtomicSequence::from(5),
+        AtomicSequence::from(15),
+    ];
+    assert_eq!(min_cursor_sequence(&sequences), 5);
+}
+
+#[test]
+fn test_min_cursor_sequence_same_values() {
+    let sequences = vec![
+        AtomicSequence::from(7),
+        AtomicSequence::from(7),
+        AtomicSequence::from(7),
+    ];
+    assert_eq!(min_cursor_sequence(&sequences), 7);
+}
+
+#[test]
+fn test_min_cursor_sequence_zero() {
+    let sequences = vec![
+        AtomicSequence::from(0),
+        AtomicSequence::from(1),
+        AtomicSequence::from(2),
+    ];
+    assert_eq!(min_cursor_sequence(&sequences), 0);
+}
