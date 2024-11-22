371b5fd02e1d4146592e1ead0d33d204e9b61f23
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-22 11:44:37 +0800

    Increased test coverage of getter macro
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_macros/tests/getters_tests.rs
+++ deep_causality_macros/tests/getters_tests.rs
@@ -24,36 +24,12 @@ fn test_basic_getters() {
 fn test_custom_getter_name() {
     #[derive(Getters)]
     struct Test {
-        #[getter(name = get_value)] // Rename getter methods as you wish
+        #[getter(name = get_value)]
         field: i32,
     }
 
     let test = Test { field: 42 };
     assert_eq!(test.get_value(), &42);
-}
-
-#[test]
-fn test_visibility() {
-    mod inner {
-        use deep_causality_macros::Getters;
-
-        #[derive(Getters)]
-        pub struct Test {
-            field1: i32,
-            field2: i32,
-        }
-
-        pub fn create_test() -> Test {
-            Test {
-                field1: 42,
-                field2: 43,
-            }
-        }
-    }
-
-    let test = inner::create_test();
-    assert_eq!(test.field1(), &42);
-    assert_eq!(test.field2(), &43);
 }
 
 #[test]
@@ -73,13 +49,83 @@ fn test_generic_types() {
 }
 
 #[test]
-fn test_multiple_attributes() {
+fn test_empty_struct() {
+    #[derive(Getters)]
+    struct Empty {}
+
+    let _empty = Empty {};
+    // Should compile without errors
+}
+
+#[test]
+fn test_multiple_custom_names() {
     #[derive(Getters)]
     struct Test {
-        #[getter(name = get_value)]
-        field: i32,
+        #[getter(name = get_first)]
+        first: i32,
+        #[getter(name = get_second)]
+        second: String,
+        third: bool, // default name
+    }
+
+    let test = Test {
+        first: 42,
+        second: "test".to_string(),
+        third: true,
+    };
+
+    assert_eq!(test.get_first(), &42);
+    assert_eq!(test.get_second(), &"test".to_string());
+    assert_eq!(test.third(), &true);
+}
+
+#[test]
+fn test_generic_with_bounds() {
+    #[derive(Getters)]
+    struct Test<T: Clone + Default> {
+        field: T,
     }
 
     let test = Test { field: 42 };
-    assert_eq!(test.get_value(), &42);
+    assert_eq!(test.field(), &42);
 }
+
+// The following tests verify compile-time errors
+
+// This should fail to compile:
+// #[test]
+// fn test_derive_on_enum() {
+//     #[derive(Getters)]
+//     enum Test {
+//         Variant1(i32),
+//         Variant2 { field: String },
+//     }
+// }
+
+// This should fail to compile:
+// #[test]
+// fn test_unnamed_fields() {
+//     #[derive(Getters)]
+//     struct Test(i32, String);
+// }
+
+// This should fail to compile:
+// #[test]
+// fn test_redundant_attributes() {
+//     #[derive(Getters)]
+//     struct Test {
+//         #[getter(name = value1)]
+//         #[getter(name = value2)]
+//         field: i32,
+//     }
+// }
+
+// This should fail to compile:
+// #[test]
+// fn test_invalid_attribute_syntax() {
+//     #[derive(Getters)]
+//     struct Test {
+//         #[getter(invalid)]
+//         field: i32,
+//     }
+// }
