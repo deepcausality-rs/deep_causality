9f8374706157c986f4bfabe8ec939b7395b4d130
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-24 18:10:46 +0800

    Fixed bench_dcl_data_structures.rs
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- dcl_data_structures/benches/bench_dcl_data_structures.rs
+++ dcl_data_structures/benches/bench_dcl_data_structures.rs
@@ -5,6 +5,8 @@ use criterion::criterion_main;
 
 mod benchmarks;
 
+// Always run these benchmarks
+#[cfg(not(feature = "unsafe"))]
 criterion_main! {
     benchmarks::ring_buffer::bit_map_benchmark::bitmap,
     benchmarks::ring_buffer::sequence_bench::sequence,
@@ -12,20 +14,21 @@ criterion_main! {
     benchmarks::grid_type::bench_grid_array::array_grid,
     benchmarks::window_type::bench_window_arr::window_array_backed,
     benchmarks::window_type::bench_window_vec::window_vector_backed,
-    // Compares the performance of the different window implementations
     benchmarks::window_type::bench_window_comp::window_impl_comp
 }
 
+// Run these benchmarks only when unsafe feature is enabled
 #[cfg(feature = "unsafe")]
 criterion_main! {
+    // Standard benchmarks
     benchmarks::ring_buffer::bit_map_benchmark::bitmap,
     benchmarks::ring_buffer::sequence_bench::sequence,
     benchmarks::ring_buffer::ring_buffer_bench::ring_buffer,
     benchmarks::grid_type::bench_grid_array::array_grid,
     benchmarks::window_type::bench_window_arr::window_array_backed,
     benchmarks::window_type::bench_window_vec::window_vector_backed,
+    benchmarks::window_type::bench_window_comp::window_impl_comp,
+    // Unsafe-specific benchmarks
     benchmarks::window_type::bench_window_unsafe_arr::window_unsafe_array_backed,
-    benchmarks::window_type::bench_window_unsafe_vec::window_unsafe_vector_backed,
-    // Compares the performance of the different window implementations
-    benchmarks::window_type::bench_window_comp::window_impl_comp
+    benchmarks::window_type::bench_window_unsafe_vec::window_unsafe_vector_backed
 }
