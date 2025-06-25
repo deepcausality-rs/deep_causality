29e262a8706dc75cc8a1db421fe384e4d5e79d7e
Marvin Hansen <marvin.hansen@gmail.com>
2025-06-25 22:22:10 +0800

    Fixed code formatting
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality/tests/types/symbolic_types/mod.rs
+++ deep_causality/tests/types/symbolic_types/mod.rs
@@ -2,5 +2,5 @@
  * SPDX-License-Identifier: MIT
  * Copyright (c) "2025" . The DeepCausality Authors and Contributors. All Rights Reserved.
  */
-mod symbolic_result_tests;
 mod symbolic_representation_tests;
+mod symbolic_result_tests;
