a1c8972e7aa2ac7cfb728b92998d415040233500
Marvin Hansen <marvin.hansen@gmail.com>
2025-07-04 15:23:03 +0800

    feat(ultragraph) working on test coverage
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- ultragraph/tests/types/ultra_graph/graph_dynamic/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-mod graph_dyn_freeze_tests;
-// #[cfg(test)]
-// mod graph_dyn_mut_tests;
-// #[cfg(test)]
-// mod graph_dyn_tests;
-// #[cfg(test)]
-// mod graph_dyn_view_tests;
--- ultragraph/tests/types/ultra_graph/mod.rs
+++ ultragraph/tests/types/ultra_graph/mod.rs
@@ -2,5 +2,7 @@
  * SPDX-License-Identifier: MIT
  * Copyright (c) "2025" . The DeepCausality Authors and Contributors. All Rights Reserved.
  */
-mod graph_dynamic;
-mod graph_static;
+
+mod graph_algo_tests;
+#[cfg(test)]
+mod graph_evolve_tests;
