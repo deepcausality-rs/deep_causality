784514a179ca7e71a0a8f8e4e4b0c9106bca5dde
Marvin Hansen <marvin.hansen@gmail.com>
2025-08-08 14:33:52 +0800

    Added unit tests for graph indexable data in context graph.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>.

---
+++ deep_causality/tests/types/context_types/context_graph/graph_indexable_data_tests.rs
@@ -0,0 +1,53 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) "2025" . The DeepCausality Authors and Contributors. All Rights Reserved.
+ */
+
+use deep_causality::*;
+
+fn get_context() -> BaseContext {
+    let id = 1;
+    let name = "base context";
+    Context::with_capacity(id, name, 10)
+}
+
+#[test]
+fn test_set_get_data_index_direct() {
+    let mut ctx = get_context();
+
+    ctx.set_data_index(0, 0, true);
+    let res = ctx.get_data_index(&0, true);
+    assert!(res.is_some());
+    let result = res.unwrap();
+    assert_eq!(result, &0);
+
+    ctx.set_data_index(0, 42, false);
+
+    let res = ctx.get_data_index(&0, false);
+    assert!(res.is_some());
+    let result = res.unwrap();
+    assert_eq!(result, &42);
+}
+
+#[test]
+fn test_set_get_current_data_index() {
+    let mut ctx = get_context();
+
+    ctx.set_current_data_index(42);
+
+    let res = ctx.get_current_data_index();
+    assert!(res.is_some());
+    let result = res.unwrap();
+    assert_eq!(result, &42);
+}
+
+#[test]
+fn test_set_get_previous_data_index() {
+    let mut ctx = get_context();
+
+    ctx.set_previous_data_index(23);
+    let res = ctx.get_previous_data_index();
+    assert!(res.is_some());
+    let result = res.unwrap();
+    assert_eq!(result, &23);
+}
--- deep_causality/tests/types/context_types/context_graph/mod.rs
+++ deep_causality/tests/types/context_types/context_graph/mod.rs
@@ -13,6 +13,8 @@ mod extendable_context_tests;
 #[cfg(test)]
 mod extendable_contextuable_graph_tests;
 #[cfg(test)]
+mod graph_indexable_data_tests;
+#[cfg(test)]
 mod graph_indexable_time_tests;
 #[cfg(test)]
 mod graph_node_tests;
