1f93edd427fab7a9353792972b8927cab9b892a5
Marvin Hansen <marvin.hansen@gmail.com>
2025-09-15 14:30:10 +0800

    feat(deep_causality_data_structures): Improved Display implementation of CausalTensor
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_data_structures/src/causal_tensor_type/mod.rs
+++ deep_causality_data_structures/src/causal_tensor_type/mod.rs
@@ -69,11 +69,15 @@ where
 impl<T: std::fmt::Display> std::fmt::Display for CausalTensor<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "CausalTensor {{ data: [")?;
-        for (i, item) in self.data.iter().enumerate() {
-            write!(f, "{}", item)?;
-            if i < self.data.len() - 1 {
+        let max_items = 10;
+        for (i, item) in self.data.iter().take(max_items).enumerate() {
+            if i > 0 {
                 write!(f, ", ")?;
             }
+            write!(f, "{}", item)?;
+        }
+        if self.data.len() > max_items {
+            write!(f, ", ...")?;
         }
         write!(
             f,
