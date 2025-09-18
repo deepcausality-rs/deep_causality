cf5f587b5a39d5d77479c9662e3b0a460d93801f
Marvin Hansen <marvin.hansen@gmail.com>
2025-09-18 15:47:56 +0800

    fix(deep_causality_num): removed default impl for trait FromPrimitive;
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_num/src/cast/from_primitive.rs
+++ deep_causality_num/src/cast/from_primitive.rs
@@ -2,7 +2,6 @@
  * SPDX-License-Identifier: MIT
  * Copyright (c) "2025" . The DeepCausality Authors and Contributors. All Rights Reserved.
  */
-use crate::cast::to_primitive::ToPrimitive;
 
 /// A generic trait for converting a number to a value.
 ///
@@ -17,31 +16,19 @@ use crate::cast::to_primitive::ToPrimitive;
 pub trait FromPrimitive: Sized {
     /// Converts an `isize` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_isize(n: isize) -> Option<Self> {
-        n.to_i64().and_then(FromPrimitive::from_i64)
-    }
+    fn from_isize(n: isize) -> Option<Self>;
 
     /// Converts an `i8` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_i8(n: i8) -> Option<Self> {
-        FromPrimitive::from_i64(From::from(n))
-    }
+    fn from_i8(n: i8) -> Option<Self>;
 
     /// Converts an `i16` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_i16(n: i16) -> Option<Self> {
-        FromPrimitive::from_i64(From::from(n))
-    }
+    fn from_i16(n: i16) -> Option<Self>;
 
     /// Converts an `i32` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_i32(n: i32) -> Option<Self> {
-        FromPrimitive::from_i64(From::from(n))
-    }
+    fn from_i32(n: i32) -> Option<Self>;
 
     /// Converts an `i64` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
@@ -49,41 +36,23 @@ pub trait FromPrimitive: Sized {
 
     /// Converts an `i128` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    ///
-    /// The default implementation converts through `from_i64()`. Types implementing
-    /// this trait should override this method if they can represent a greater range.
-    #[inline]
-    fn from_i128(n: i128) -> Option<Self> {
-        n.to_i64().and_then(FromPrimitive::from_i64)
-    }
+    fn from_i128(n: i128) -> Option<Self>;
 
     /// Converts a `usize` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_usize(n: usize) -> Option<Self> {
-        n.to_u64().and_then(FromPrimitive::from_u64)
-    }
+    fn from_usize(n: usize) -> Option<Self>;
 
     /// Converts an `u8` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_u8(n: u8) -> Option<Self> {
-        FromPrimitive::from_u64(From::from(n))
-    }
+    fn from_u8(n: u8) -> Option<Self>;
 
     /// Converts an `u16` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_u16(n: u16) -> Option<Self> {
-        FromPrimitive::from_u64(From::from(n))
-    }
+    fn from_u16(n: u16) -> Option<Self>;
 
     /// Converts an `u32` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_u32(n: u32) -> Option<Self> {
-        FromPrimitive::from_u64(From::from(n))
-    }
+    fn from_u32(n: u32) -> Option<Self>;
 
     /// Converts an `u64` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
@@ -91,32 +60,13 @@ pub trait FromPrimitive: Sized {
 
     /// Converts an `u128` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    ///
-    /// The default implementation converts through `from_u64()`. Types implementing
-    /// this trait should override this method if they can represent a greater range.
-    #[inline]
-    fn from_u128(n: u128) -> Option<Self> {
-        n.to_u64().and_then(FromPrimitive::from_u64)
-    }
+    fn from_u128(n: u128) -> Option<Self>;
 
     /// Converts a `f32` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    #[inline]
-    fn from_f32(n: f32) -> Option<Self> {
-        FromPrimitive::from_f64(From::from(n))
-    }
+    fn from_f32(n: f32) -> Option<Self>;
 
     /// Converts a `f64` to return an optional value of this type. If the
     /// value cannot be represented by this type, then `None` is returned.
-    ///
-    /// The default implementation tries to convert through `from_i64()`, and
-    /// failing that through `from_u64()`. Types implementing this trait should
-    /// override this method if they can represent a greater range.
-    #[inline]
-    fn from_f64(n: f64) -> Option<Self> {
-        match n.to_i64() {
-            Some(i) => FromPrimitive::from_i64(i),
-            None => n.to_u64().and_then(FromPrimitive::from_u64),
-        }
-    }
+    fn from_f64(n: f64) -> Option<Self>;
 }
