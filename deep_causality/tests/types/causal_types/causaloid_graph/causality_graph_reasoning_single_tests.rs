f49141940afe80c9b516470cc344b22afdbabc82
Marvin Hansen <marvin.hansen@gmail.com>
2025-11-16 12:17:06 +0800

    test(deep_causality): Updated more tests to new API and re-write missing tests.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality/tests/types/causal_types/causaloid_graph/causality_graph_reasoning_single_tests.rs
+++ deep_causality/tests/types/causal_types/causaloid_graph/causality_graph_reasoning_single_tests.rs
@@ -9,42 +9,52 @@ use deep_causality::*;
 #[test]
 fn test_evaluate_single_cause_success() {
     let mut g = CausaloidGraph::new(0);
-    let causaloid = test_utils::get_test_causaloid_deterministic();
+    let causaloid = test_utils::get_test_causaloid_deterministic(0);
     let index = g.add_causaloid(causaloid).expect("Failed to add causaloid");
     g.freeze(); // Reasoning requires a frozen graph
-    // Evaluate the node using the high-level graph API.
-    let effect = PropagatingEffect::Numerical(0.99);
-    let res = g.evaluate_single_cause(index, &effect);
 
+    // Evaluate the node using the high-level graph API.
+    let effect = PropagatingEffect::from_numerical(0.99);
+    let res = g.evaluate_single_cause(index, &effect);
+    dbg!(&res);
     assert!(res.is_ok());
-    assert_eq!(res.unwrap(), PropagatingEffect::Deterministic(true));
+    assert_eq!(res.value, EffectValue::Deterministic(true));
 }
 
 #[test]
-fn test_evaluate_single_cause_error_conditions() {
-    let effect = PropagatingEffect::Numerical(0.99);
-
+fn test_evaluate_single_causaloid_not_found_error() {
     // Case 1: Node does not exist in the graph.
-    let g: BaseCausalGraph = CausaloidGraph::new(0);
+    let mut g: BaseCausalGraph = CausaloidGraph::new(0);
     let non_existent_index = 99;
+    g.freeze(); // Reasoning requires a frozen graph
+
+    let effect = PropagatingEffect::from_numerical(0.99);
     let res = g.evaluate_single_cause(non_existent_index, &effect);
+    dbg!(&res);
 
     assert!(res.is_err());
-    assert_eq!(
-        res.unwrap_err().to_string(),
-        "CausalityError: Causaloid with index 99 not found in graph"
+    assert!(
+        res.error
+            .unwrap()
+            .to_string()
+            .contains("CausalityError: Causaloid with index 99 not found in graph"),
     );
+}
 
+#[test]
+fn test_evaluate_single_eval_error() {
     // Case 2: The causaloid itself returns an error during evaluation.
     let mut g = CausaloidGraph::new(0);
     let error_causaloid = test_utils::get_test_error_causaloid();
     let index = g
         .add_causaloid(error_causaloid)
         .expect("Failed to add causaloid");
-    g.freeze();
+    g.freeze(); // Reasoning requires a frozen graph
 
+    let effect = PropagatingEffect::from_deterministic(false);
     let res = g.evaluate_single_cause(index, &effect);
+    dbg!(&res);
 
     assert!(res.is_err());
-    assert_eq!(res.unwrap_err().to_string(), "CausalityError: Test error");
+    assert_eq!(res.error.unwrap().to_string(), "CausalityError: Test error");
 }
--- deep_causality/tests/types/causal_types/causaloid_graph/mod.rs
+++ deep_causality/tests/types/causal_types/causaloid_graph/mod.rs
@@ -11,9 +11,10 @@ mod causality_graph_freeze_tests;
 mod causality_graph_nodes_tests;
 #[cfg(test)]
 mod causality_graph_reasoning_all_tests;
-
 #[cfg(test)]
 mod causality_graph_reasoning_linear_tests;
+#[cfg(test)]
+mod causality_graph_reasoning_single_tests;
 
 // #[cfg(test)]
 // mod causality_graph_reasoning_imbalanced_tests;
@@ -21,8 +22,6 @@ mod causality_graph_reasoning_linear_tests;
 // mod causality_graph_reasoning_multi_tests;
 // #[cfg(test)]
 // mod causality_graph_reasoning_shortest_tests;
-// #[cfg(test)]
-// mod causality_graph_reasoning_single_tests;
 // #[cfg(test)]
 // mod causality_graph_reasoning_sub_tests;
 
