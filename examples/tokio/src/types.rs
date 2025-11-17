e78dbc57a2eebf8c5f6aebde0becb2878b4e46e1
Marvin Hansen <marvin.hansen@gmail.com>
2025-11-17 13:34:57 +0800

    feat(deep_causality): Updated starter example to new API.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- examples/tokio/src/types.rs
+++ examples/tokio/src/types.rs
@@ -7,6 +7,17 @@ use deep_causality::{
     NumericalValue,
 };
 
+// # Model Type Parameters
+// - `I`: The type of the input effect value, must implement `IntoEffectValue`.
+// - `O`: The type of the output effect value, must implement `IntoEffectValue`.
+// -- These are only relevant when using context.
+// - `D`: The type for data context, must implement `Datable` and `Clone`.
+// - `S`: The type for spatial context, must implement `Spatial<VS>` and `Clone`.
+// - `T`: The type for temporal context, must implement `Temporal<VT>` and `Clone`.
+// - `ST`: The type for spatiotemporal context, must implement `SpaceTemporal<VS, VT>` and `Clone`.
+// - `SYM`: The type for symbolic context, must implement `Symbolic` and `Clone`.
+// - `VS`: The value type for spatial data, must implement `Clone`.
+// - `VT`: The value type for temporal data, must implement `Clone`.
 pub type BaseModelTokio = Model<
     NumericalValue,
     bool,
