77fe947e7b843aefe4f2008498cffe131d3706d5
Marvin Hansen <marvin.hansen@gmail.com>
2026-01-06 20:07:25 +0800

    feat(deep_causality_topology): Fixed some tests.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_topology/tests/extensions/hkt_curvature_tests.rs
+++ deep_causality_topology/tests/extensions/hkt_curvature_tests.rs
@@ -11,16 +11,22 @@ use deep_causality_topology::{
 
 #[test]
 fn test_geodesic_deviation_flat() {
-    let flat: CurvatureTensor<f64, TensorVector, TensorVector, TensorVector, TensorVector> =
-        CurvatureTensor::flat(4);
+    let flat: CurvatureTensor<
+        f64,
+        TensorVector<f64>,
+        TensorVector<f64>,
+        TensorVector<f64>,
+        TensorVector<f64>,
+    > = CurvatureTensor::flat(4);
 
-    let u = TensorVector::basis(4, 0);
-    let v = TensorVector::basis(4, 1);
-    let w = TensorVector::new(&[1.0, 2.0, 3.0, 4.0]);
+    let u = TensorVector::<f64>::basis(4, 0);
+    let v = TensorVector::<f64>::basis(4, 1);
+    let w = TensorVector::<f64>::new(&[1.0, 2.0, 3.0, 4.0]);
 
     // Fully qualified path required due to HKT trait complexity
-    let deviation: TensorVector =
-        <CurvatureTensorWitness as RiemannMap<CurvatureTensorWitness>>::curvature(flat, u, v, w);
+    let deviation: TensorVector<f64> = <CurvatureTensorWitness<f64> as RiemannMap<
+        CurvatureTensorWitness<f64>,
+    >>::curvature(flat, u, v, w);
 
     // Flat spacetime has zero geodesic deviation
     assert!(deviation.data.iter().all(|&x: &f64| x.abs() < f64::EPSILON));
@@ -28,10 +34,10 @@ fn test_geodesic_deviation_flat() {
 
 #[test]
 fn test_tensor_vector_operations() {
-    let v = TensorVector::new(&[1.0, 2.0, 3.0]);
+    let v = TensorVector::<f64>::new(&[1.0, 2.0, 3.0]);
     assert_eq!(v.dim(), 3);
 
-    let basis = TensorVector::basis(4, 2);
+    let basis = TensorVector::<f64>::basis(4, 2);
     assert_eq!(basis.data[2], 1.0);
     assert_eq!(basis.data[0], 0.0);
 }
@@ -39,26 +45,32 @@ fn test_tensor_vector_operations() {
 #[test]
 fn test_curved_tensor_contraction() {
     // Create a simple non-flat curvature tensor
-    let tensor: CurvatureTensor<f64, TensorVector, TensorVector, TensorVector, TensorVector> =
-        CurvatureTensor::from_generator(
-            2,
-            Metric::Euclidean(2),
-            CurvatureSymmetry::None,
-            |d, a, b, c| {
-                if d == 0 && a == 0 && b == 1 && c == 0 {
-                    1.0 // R^0_010 = 1
-                } else {
-                    0.0
-                }
-            },
-        );
+    let tensor: CurvatureTensor<
+        f64,
+        TensorVector<f64>,
+        TensorVector<f64>,
+        TensorVector<f64>,
+        TensorVector<f64>,
+    > = CurvatureTensor::from_generator(
+        2,
+        Metric::Euclidean(2),
+        CurvatureSymmetry::None,
+        |d, a, b, c| {
+            if d == 0 && a == 0 && b == 1 && c == 0 {
+                1.0 // R^0_010 = 1
+            } else {
+                0.0
+            }
+        },
+    );
 
-    let u = TensorVector::new(&[1.0, 0.0]);
-    let v = TensorVector::new(&[0.0, 1.0]);
-    let w = TensorVector::new(&[1.0, 0.0]);
+    let u = TensorVector::<f64>::new(&[1.0, 0.0]);
+    let v = TensorVector::<f64>::new(&[0.0, 1.0]);
+    let w = TensorVector::<f64>::new(&[1.0, 0.0]);
 
-    let result: TensorVector =
-        <CurvatureTensorWitness as RiemannMap<CurvatureTensorWitness>>::curvature(tensor, u, v, w);
+    let result: TensorVector<f64> = <CurvatureTensorWitness<f64> as RiemannMap<
+        CurvatureTensorWitness<f64>,
+    >>::curvature(tensor, u, v, w);
 
     // R(u,v)w with R^0_010 = 1 should give [1, 0]
     // u=0 -> a=0
@@ -77,19 +89,26 @@ fn test_curved_tensor_contraction() {
 fn test_scatter_vectors() {
     // Test basic S-matrix placeholder logic
     // Interaction tensor with 1.0 everywhere
-    let tensor: CurvatureTensor<f64, TensorVector, TensorVector, TensorVector, TensorVector> =
-        CurvatureTensor::from_generator(
-            2,
-            Metric::Euclidean(2),
-            CurvatureSymmetry::None,
-            |_, _, _, _| 1.0,
+    let tensor: CurvatureTensor<
+        f64,
+        TensorVector<f64>,
+        TensorVector<f64>,
+        TensorVector<f64>,
+        TensorVector<f64>,
+    > = CurvatureTensor::from_generator(
+        2,
+        Metric::Euclidean(2),
+        CurvatureSymmetry::None,
+        |_, _, _, _| 1.0,
+    );
+
+    let in1 = TensorVector::<f64>::new(&[1.0, 0.0]);
+    let in2 = TensorVector::<f64>::new(&[1.0, 0.0]);
+
+    let (out1, out2): (TensorVector<f64>, TensorVector<f64>) =
+        <CurvatureTensorWitness<f64> as RiemannMap<CurvatureTensorWitness<f64>>>::scatter(
+            tensor, in1, in2,
         );
-
-    let in1 = TensorVector::new(&[1.0, 0.0]);
-    let in2 = TensorVector::new(&[1.0, 0.0]);
-
-    let (out1, out2): (TensorVector, TensorVector) =
-        <CurvatureTensorWitness as RiemannMap<CurvatureTensorWitness>>::scatter(tensor, in1, in2);
 
     // If all components 1.0:
     // amplitude = 1.0 * 1.0 * 1.0 = 1.0 (since only a=0,b=0 nonzero)
