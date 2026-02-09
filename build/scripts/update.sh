c586b89255db441765e201db893e4c95b75fb69b
Marvin Hansen <marvin.hansen@gmail.com>
2026-02-09 21:12:42 +0800

    updated all cargo dependencies to the latest version.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ build/scripts/update.sh
@@ -0,0 +1,9 @@
+#
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2023 - 2026. The DeepCausality Authors and Contributors. All Rights Reserved.
+#
+set -o errexit
+set -o nounset
+set -o pipefail
+
+command cargo update
