29361c1b7e2713a6b5f75479b9f8c1855a6bce11
Marvin Hansen <marvin.hansen@gmail.com>
2026-02-10 15:14:53 +0800

    Added hardware secure CSPRNG to rand crate
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_rand/src/types/mod.rs
+++ deep_causality_rand/src/types/mod.rs
@@ -11,4 +11,7 @@ pub mod range;
 #[cfg(feature = "os-random")]
 pub use rand::os_random_rng::OsRandomRng;
 
+#[cfg(feature = "aead-random")]
+pub use rand::chacha_rng::ChaCha20Rng;
+
 pub use rand::std_rng::Xoshiro256;
