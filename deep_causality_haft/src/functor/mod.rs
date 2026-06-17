858e949714f1db8506033f75023f8c2ff55ea2be
Marvin Hansen <marvin.hansen@gmail.com>
2026-06-17 13:24:46 +0800

    feat(deep_causality_haft): formatting
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_haft/src/functor/mod.rs
+++ deep_causality_haft/src/functor/mod.rs
@@ -2,7 +2,6 @@
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2023 - 2026. The DeepCausality Authors and Contributors. All Rights Reserved.
  */
-use crate::{HKT, Satisfies};
 
 pub(crate) mod bifunctor;
 pub(crate) mod functor_base;
