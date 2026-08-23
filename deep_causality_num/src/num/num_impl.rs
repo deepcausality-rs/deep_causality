a25a00cab9cfcdc07df90bfc06b98dfde53f625a
Marvin Hansen <marvin.hansen@gmail.com>
2026-08-23 18:18:57 +0800

    fix(deep_causality_num):  Added u64 and i64 default implementation to num trait
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- deep_causality_num/src/num/num_impl.rs
+++ deep_causality_num/src/num/num_impl.rs
@@ -8,12 +8,14 @@ impl Num for usize {}
 impl Num for u8 {}
 impl Num for u16 {}
 impl Num for u32 {}
+impl Num for u64 {}
 impl Num for u128 {}
 
 impl Num for isize {}
 impl Num for i8 {}
 impl Num for i16 {}
 impl Num for i32 {}
+impl Num for i64 {}
 impl Num for i128 {}
 
 impl Num for f32 {}
