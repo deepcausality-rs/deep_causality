79103335bd4c0c6f23f927dfcc768028938033dc
Marvin Hansen <marvin.hansen@gmail.com>
2026-09-06 19:41:42 +0800

    feat(make): Added LoC count script and make target.
    Closes issue https://github.com/deepcausality-rs/deep_causality/issues/772
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ scripts/count.sh
@@ -0,0 +1,11 @@
+#
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2023 - 2026. The DeepCausality Authors and Contributors. All Rights Reserved.
+#
+set -o errexit
+set -o nounset
+set -o pipefail
+
+command echo ""
+command echo "Count LoC"
+command tokei
